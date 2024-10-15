3e3ba06bfb93b02705d65ed40dadf2b440dc3eb7
Yazmin de la Fuente <lyazmin72@gmail.com>
2024-10-15 07:39:21 -0600

    Parcial02 - Add validation error section

--- client/src/app/errors/test-errors/test.errors.component.ts
+++ client/src/app/errors/test-errors/test.errors.component.ts
@@ -7,37 +7,47 @@ import { Component, inject } from '@angular/core';
   templateUrl: './test-errors.component.html',
   styleUrl: './test-errors.component.css'
 })
+
 export class TestErrorsComponent {
   baseUrl = "https://localhost:5001/api/";
   private http = inject(HttpClient);
+  validationErrors: string[] = [];
+  
   get400Error(): void {
     this.http.get(this.baseUrl + "buggy/bad-request").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+  
   get401Error(): void {
     this.http.get(this.baseUrl + "buggy/auth").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+  
   get404Error(): void {
     this.http.get(this.baseUrl + "buggy/not-found").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+  
   get500Error(): void {
     this.http.get(this.baseUrl + "buggy/server-error").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+  
   get400ValidationError(): void {
     this.http.post(this.baseUrl + "account/register", {}).subscribe({
       next: (response) => console.log(response),
-      error: (error) => console.log(error)
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      }
     })
   }
 }
