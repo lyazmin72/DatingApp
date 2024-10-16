b71c1510b2c81d0bb955833c62c191ed45619abc
Yazmin de la Fuente <lyazmin72@gmail.com>
2024-10-16 09:12:17 -0600

    Parcial02 - Add server-error handling

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-server-error',
   standalone: true,
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './server-error.component.css'
 })
 export class ServerErrorComponent {
+  error: any;
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.["error"];
+  }
 }
