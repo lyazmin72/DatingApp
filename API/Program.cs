d93b81a318aeb1d85a306b350079fc1444978667
Yazmin de la Fuente <lyazmin72@gmail.com>
2024-10-18 08:06:35 -0600

    Parcial02 - Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using API.Data;
 using API.Extensions;
 using API.Middlewares;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -9,8 +12,26 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors((cors) => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseCors((cors) =>
+    cors.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsersAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration/seeding.");
+}
+
 app.Run();
