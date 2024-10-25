2a4d51aa8bf7740815e01cde7de4438e356e515d
Yazmin de la Fuente <lyazmin72@gmail.com>
2024-10-25 07:41:46 -0600

    Parcial03 - Add style to member cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 @Component({
   selector: 'app-member-card',
   standalone: true,
   imports: [],
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class MemberCardComponent {
   member = input.required<Member>();
