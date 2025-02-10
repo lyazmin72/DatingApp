ded0381dff3c981d8861f295a674bb16d42d02b7
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-10 15:10:27 -0600

    Parcial014 - Add photo to nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -41,8 +41,9 @@ public class AccountController(
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == request.Username.ToLower());
-
+        var user = await context.Users
+            .Include(x => x.Photos)
+            .FirstOrDefaultAsync(x => x.UserName.ToLower() == request.Username.ToLower());
         if (user == null)
         {
             return Unauthorized("Invalid username or password");
@@ -61,7 +62,8 @@ public class AccountController(
         return new UserResponse
         {
             Username = user.UserName,
-            Token = tokenService.CreateToken(user)
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
         };
     }
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -15,8 +15,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
       map((user) => {
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -26,14 +25,18 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
       map((user) => {
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
     );
   }
   
+  setCurrentUser(user: User) {
+    localStorage.setItem("user", JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
   logout(): void {
     localStorage.removeItem("user");
     this.currentUser.set(null);
