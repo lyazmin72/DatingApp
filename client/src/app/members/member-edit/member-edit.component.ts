c39c678be2dc410edef648fb7ccc8cbcbf7fe9c1
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-10 13:02:26 -0600

    Parcial04 - Add photo uploader

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -42,4 +42,8 @@ export class MemberEditComponent implements OnInit {
     this.toastr.success("Profile updated!");
     this.editForm?.reset(this.member);
   }
+
+  onMemberChange(event: Member) {
+    this.member = event;
+  }
 }
