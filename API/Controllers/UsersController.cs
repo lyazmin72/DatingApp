b82b292bc1f7cd84e5303e9caebb1dce004b0b09
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-12 07:54:54 -0600

    Parcial04 - Delete photo from API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -82,6 +82,7 @@ public class UsersController : BaseApiController
         }
         return BadRequest("Problem adding the photo");
     }
+
     [HttpPut("photo/{photoId:int}")]
     public async Task<ActionResult> SetPhotoAsMain(int photoId)
     {
@@ -94,5 +95,22 @@ public class UsersController : BaseApiController
         photo.IsMain = true;
         if (await _repository.SaveAllAsync()) return NoContent();
         return BadRequest("There was a problem.");
+    }
+
+    [HttpDelete("photo/{photoId:int}")]
+    public async Task<ActionResult> DeletePhoto(int photoId)
+    {
+        var user = await _repository.GetByUsernameAsync(User.GetUserName());
+        if (user == null) return BadRequest("User not found");
+        var photo = user.Photos.FirstOrDefault(p => p.Id == photoId);
+        if (photo == null || photo.IsMain) return BadRequest("This photo can't be deleted");
+        if (photo.PublicId != null)
+        {
+            var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+            if (result.Error != null) return BadRequest(result.Error.Message);
+        }
+        user.Photos.Remove(photo);
+        if (await _repository.SaveAllAsync()) return Ok();
+        return BadRequest("There was a problem when deleting the photo");
     }
 }
