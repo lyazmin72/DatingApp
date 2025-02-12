8b67e1a0f36c66061e23d1fbb7afe9e9695f3272
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-12 07:48:56 -0600

    Parcial04 - Set main photo in client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -4,6 +4,8 @@ import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
+import { MembersService } from '../../_services/members.service';
+import { Photo } from '../../_models/photo';
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
@@ -13,17 +15,41 @@ import { environment } from '../../../environments/environment';
 })
 export class PhotoEditorComponent implements OnInit {
   private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+
   member = input.required<Member>();
   uploader?: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   memberChange = output<Member>();
+  
   ngOnInit(): void {
     this.initializeUploader();
   }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: _ => {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        const updatedMember = { ...this.member() };
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id == photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      }
+    });
+  }
+  
   fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
+  
   initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + "users/photo",
