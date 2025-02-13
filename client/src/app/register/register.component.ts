b81778250e3354d651436d2fe613d1d16d971225
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-13 12:34:27 -0600

    Parcial04 - Initialize Reactive form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,34 +1,47 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   cancelRegister = output<boolean>();
   private accountService = inject(AccountService);
-  // usersFromHomeComponent = input.required<any>();
   private toastr = inject(ToastrService);
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
-  register(): void {
-    this.accountService.register(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => {
-        console.log(error);
-        this.toastr.error(error.errors);
-      }
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
     });
   }
+
+  register(): void {
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: (response) => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //     this.toastr.error(error.errors);
+    //   }
+    // });
+  }
+
   cancel(): void {
     this.cancelRegister.emit(false);
   }
