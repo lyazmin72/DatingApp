17076c31c285f6b9b004f5286bfa28021e8c5042
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-02-26 22:31:20 -0600

    Parcial05 - Add pagination implementation in controller

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.DataEntities;
 using API.DTOs;
+using API.Helpers;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
@@ -26,10 +27,11 @@ public class UserRepository(DataContext context, IMapper mapper) : IUserReposito
                 .Where(u => u.UserName == username)
                 .ProjectTo<MemberResponse>(mapper.ConfigurationProvider)
                 .SingleOrDefaultAsync();
-    public async Task<IEnumerable<MemberResponse>> GetMembersAsync()
-        => await context.Users
-                .ProjectTo<MemberResponse>(mapper.ConfigurationProvider)
-                .ToListAsync();
+    public async Task<PagedList<MemberResponse>> GetMembersAsync(UserParams userParams)
+    {
+        var query = context.Users.ProjectTo<MemberResponse>(mapper.ConfigurationProvider);
+        return await PagedList<MemberResponse>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+    }
 
     public async Task<bool> SaveAllAsync()
         => await context.SaveChangesAsync() > 0;
