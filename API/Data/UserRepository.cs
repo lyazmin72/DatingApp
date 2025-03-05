feffb94a773bff8bb4031d19ebca692bdb38789d
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-03-04 21:03:29 -0600

    Parcial05 - Add sort by to the API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 namespace API.Data;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using API.DataEntities;
 using API.DTOs;
@@ -42,6 +43,11 @@ public class UserRepository(DataContext context, IMapper mapper) : IUserReposito
         var maxBDay = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
 
         query = query.Where(u => u.BirthDay >= minBDay && u.BirthDay <= maxBDay);
+        query = userParams.OrderBy.ToLower(CultureInfo.InvariantCulture) switch
+        {
+            "created" => query.OrderByDescending(x => x.Created),
+            _ => query.OrderByDescending(x => x.LastActive)
+        };
 
         return await PagedList<MemberResponse>.CreateAsync(
             query.ProjectTo<MemberResponse>(mapper.ConfigurationProvider), userParams.PageNumber, userParams.PageSize);
