bfff6f6ba753a40623b1cdefde3a35c2c01f615c
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-03-11 20:17:10 -0600

    Parcial05 - Add UserLike migration

--- API/DataEntities/AppUser.cs
+++ API/DataEntities/AppUser.cs
@@ -1,6 +1,5 @@
 namespace API.DataEntities;
 
-using API.Extensions;
 public class AppUser
 {
     public int Id { get; set; }
@@ -20,5 +19,7 @@ public class AppUser
     public List<Photo> Photos { get; set; } = [];
 
     // public int GetAge() => BirthDay.CalculateAge();
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
 
 }
