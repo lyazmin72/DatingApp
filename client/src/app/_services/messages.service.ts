763b1a731a698f220272807bfb45def6e6e6f67e
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-04-15 21:29:11 -0600

    Parcial05 - Fix to messageS/thread, in PLURAL, see messageS service line 27

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -15,8 +15,8 @@ import { inject, Injectable, signal } from '@angular/core';
  
    getMessages(pageNumber: number, pageSize: number, container: string) {
      let params = setPaginationHeaders(pageNumber, pageSize);
-     params = params.append("Container", container);
- 
+     params = params.append("Container", container.toLocaleLowerCase());
+     
      return this.http.get<Message[]>(this.baseUrl + "messages",
        { observe: "response", params }).subscribe({
          next: response => setPaginationResponse(response, this.paginatedResult)
