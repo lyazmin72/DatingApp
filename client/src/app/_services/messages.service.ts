00e9e017331ab9ac7dcf237fee2ed6031801adbd
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-04-15 23:16:57 -0600

    Parcial05 - Send messages

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -16,7 +16,7 @@ import { inject, Injectable, signal } from '@angular/core';
    getMessages(pageNumber: number, pageSize: number, container: string) {
      let params = setPaginationHeaders(pageNumber, pageSize);
      params = params.append("Container", container.toLocaleLowerCase());
-     
+
      return this.http.get<Message[]>(this.baseUrl + "messages",
        { observe: "response", params }).subscribe({
          next: response => setPaginationResponse(response, this.paginatedResult)
@@ -25,5 +25,8 @@ import { inject, Injectable, signal } from '@angular/core';
 
    getMessageThread(username: string) {
     return this.http.get<Message[]>(this.baseUrl + "message/thread/" + username);
+  }
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
   }
  }
