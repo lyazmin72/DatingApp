977cdd9e5d91021149d79e0d5ca9327368044852
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-04-15 21:22:07 -0600

    Parcial05 - Add message thread to SPA

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -22,4 +22,8 @@ import { inject, Injectable, signal } from '@angular/core';
          next: response => setPaginationResponse(response, this.paginatedResult)
        });
    }
+
+   getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + "message/thread/" + username);
+  }
  }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -15,7 +15,7 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 })
 export class MessagesComponent implements OnInit {
   messagesService = inject(MessagesService);
-  container = "unread";
+  container = "Inbox";
   pageNumber = 1;
   pageSize = 10;
 
@@ -31,9 +31,9 @@ export class MessagesComponent implements OnInit {
     // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
     // else return `/members/${message.senderUsername}`;
   
-    return this.container === "outbox"
-      ? `/members/${message.recipientUsername}`
-      : `/members/${message.senderUsername}`;
+    return this.container === "Outbox"
+    ? `/members/${message.recipientUsername}`
+    : `/members/${message.senderUsername}`;
   }
   
 
