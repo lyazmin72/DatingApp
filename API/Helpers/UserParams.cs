3b9457aaee92d0077a763448b923bd355b7ab691
Yazmin de la Fuente <75684424+lyazmin72@users.noreply.github.com>
2025-05-06 15:02:42 -0600

    Parcial05
    * Parcial05 - Add pagination implementation in controller
    
    * Parcial05 - Add client pagination Part 1
    
    * Parcial05 - Add client pagination Part 2
    
    * Parcial05 - Add initial setup for more filters
    
    * Parcial05 - Add additional filters
    
    * Parcial05 - Clean up member service
    
    * Parcial05 - Add filter buttons to SPA
    
    * Parcial05 - Add sort by to the API
    
    * Parcial05 - Add sort by to the SPA
    
    * Parcial05 - Add ActionFilter
    
    * Parcial05 - Optimize action filter
    
    * Parcial05 - Add TimeAgo module for date formatting
    
    * Parcial05 - Restore members cache
    
    * Parcial05 - Restore cache for member details and persisted filters in members service
    
    * Parcial05 - Add UserLike migration
    
    * Parcial05 - Add LikesRepository
    
    * Parcial05 - Add Likes Controller
    
    * Parcial05 - Add Like feature in SPA
    
    * Parcial05 - Add pagination for Likes
    
    * Parcial05 - Add likes pagination to SPA
    
    * Parcial05 - Add Message EF Migration
    
    * Parcial05 - Add Message Repository
    
    * Parcial05 - Add AutoMapperProfile for message response
    
    * Parcial05 - Add Message Controller
    
    * Parcial05 - Add Get Messages for User
    
    * Parcial05 - Add Message Thread to controller
    
    * Parcial05 - Add MessagesService to SPA
    
    * Parcial05 - Add Message Inbox 1 of 2
    
    * Parcial05 - Add message inbox
    
    * Parcial05 - Add message thread to SPA
    
    * Parcial05 - Fix to messageS/thread, in PLURAL, see messageS service line 27
    
    * Parcial05 - Add style to messages
    
    * Parcial05 - Add Activated Tab
    
    * Parcial05 - Add query params for tabs
    
    * Parcial05 - Add Route resolver
    
    * Parcial05 - Send messages
    
    * Parcial05 - Fix some small bugs
    
    * Parcial05 - Add deletion of messages in API
    
    * Parcial05 - Add deletion of message in SPA

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -1,14 +1,11 @@
 namespace API.Helpers;
 
-public class UserParams
+public class UserParams : PaginationParams
 {
-    private const int MAX_PAGE_SIZE = 50;
-    public int PageNumber { get; set; } = 1;
+    public string? CurrentUsername { get; set; }
+    public string? Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 100;
+    public string OrderBy { get; set; } = "lastActive";
 
-    private int pageSize = 10;
-    public int PageSize
-    {
-        get => pageSize;
-        set => pageSize = (value > MAX_PAGE_SIZE) ? MAX_PAGE_SIZE : value;
-    }
 }
