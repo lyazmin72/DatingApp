f76da554288bbcf5bf9ce638489efef780b65661
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-05-12 22:39:45 -0600

    Parcial06 - Update the seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,27 +2,29 @@ namespace API.Data;
 using System.Security.Cryptography;
 using System.Text.Json;
 using API.DataEntities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 public class Seed
 {
-    public static async Task SeedUsersAsync(DataContext context)
+    public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
     {
-        if (await context.Users.AnyAsync())
+        if (await userManager.Users.AnyAsync())
         {
             return;
         }
         var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, ReadOptions);
         if (users == null)
         {
             return;
         }
         foreach (var user in users)
         {
-            using var hmac = new HMACSHA512();
-            context.Users.Add(user);
+            await userManager.CreateAsync(user, "Pa$$w0rd");
         }
-        await context.SaveChangesAsync();
     }
+    private static readonly JsonSerializerOptions ReadOptions = new()
+    {
+        AllowTrailingCommas = true
+    };
 }
