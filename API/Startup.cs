994086c11ab1318e1f4604c681e1464a5b8aca21
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-05-25 20:41:57 -0600

    Parcial06 - Add send message to Message Hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,7 +38,7 @@ public class Startup
         {
             endpoints.MapControllers();
             endpoints.MapHub<PresenceHub>("hubs/presence");
-            endpoints.MapHub<MessageHub>("hubs/message");
+            endpoints.MapHub<MessageHub>("hubs/messages");
         });
     }
 }
