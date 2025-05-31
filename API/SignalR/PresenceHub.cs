b1f240fbad405efd5911353737cef91ff0403818
Yazmin de la Fuente <lyazmin72@gmail.com>
2025-05-30 18:44:23 -0600

    Parcial06 - Optimize presence

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -14,10 +14,13 @@ public class PresenceHub(PresenceTracker presenceTracker) : Hub
             throw new HubException("Cannot get the current user claim");
         }
 
-        await presenceTracker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
-        await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserName());
-
-        await GetOnlineUsersAsync();
+        var isOnline = await presenceTracker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
+        if (isOnline)
+        {
+            await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserName());
+        }
+        var onlineUsers = await presenceTracker.GetOnlineUsers();
+        await Clients.Caller.SendAsync("GetOnlineUsers", onlineUsers);
     }
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
@@ -26,15 +29,11 @@ public class PresenceHub(PresenceTracker presenceTracker) : Hub
             throw new HubException("Cannot get the current user claim");
         }
 
-        await presenceTracker.UserDisconnected(Context.User.GetUserName(), Context.ConnectionId);
-        await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUserName());
-        await GetOnlineUsersAsync();
-
+        var isOffline = await presenceTracker.UserDisconnected(Context.User.GetUserName(), Context.ConnectionId);
+        if (isOffline)
+        {
+            await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUserName());
+        }
         await base.OnDisconnectedAsync(exception);
-    }
-    private async Task GetOnlineUsersAsync()
-    {
-        var onlineUsers = await presenceTracker.GetOnlineUsers();
-        await Clients.All.SendAsync("GetOnlineUsers", onlineUsers);
     }
 }
--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -4,8 +4,9 @@ public class PresenceTracker
 {
     private static readonly Dictionary<string, List<string>> OnlineUsers = [];
 
-    public Task UserConnected(string username, string connectionId)
+    public Task<bool> UserConnected(string username, string connectionId)
     {
+        var isOnline = false;
         lock (OnlineUsers)
         {
             if (OnlineUsers.ContainsKey(username))
@@ -15,19 +16,22 @@ public class PresenceTracker
             else
             {
                 OnlineUsers.Add(username, [connectionId]);
+                isOnline = true;
+
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOnline);
     }
 
-    public Task UserDisconnected(string username, string connectionId)
+    public Task<bool> UserDisconnected(string username, string connectionId)
     {
+        var isOffline = false;
         lock (OnlineUsers)
         {
             if (!OnlineUsers.ContainsKey(username))
             {
-                return Task.CompletedTask;
+                return Task.FromResult(isOffline);
             }
 
             OnlineUsers[username].Remove(connectionId);
@@ -35,10 +39,11 @@ public class PresenceTracker
             if (OnlineUsers[username].Count == 0)
             {
                 OnlineUsers.Remove(username);
+                isOffline = true;
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOffline);
     }
 
     public Task<string[]> GetOnlineUsers()
